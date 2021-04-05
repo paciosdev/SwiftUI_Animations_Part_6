1917ba09de9c597707ed86c78ff41cf408b5a0e4
Francesco Paciello <kekkopacio@gmail.com>
2021-04-05 22:11:18 +0200

    Initial Commit

---
+++ SpriteKit Animation/SpriteKit_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpriteKit_AnimationApp.swift
+//  SpriteKit Animation
+//
+//  Created by Kekko Paciello on 05/04/21.
+//
+
+import SwiftUI
+
+@main
+struct SpriteKit_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
