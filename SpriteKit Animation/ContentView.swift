f99260c9e506f5166026b3ad2285bba64b96a862
Francesco Paciello <kekkopacio@gmail.com>
2021-04-06 16:23:09 +0200

    first commit

--- SpriteKit Animation/ContentView.swift
+++ SpriteKit Animation/ContentView.swift
@@ -6,11 +6,51 @@
 //
 
 import SwiftUI
+import SpriteKit
+
+class GameScene: SKScene{
+    override func didMove(to view: SKView) {
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let location = touch.location(in: self)
+        let ball = SKSpriteNode(imageNamed: "paciosdev")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.7)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else {return}
+        let location = touch.location(in: self)
+        let ball = SKSpriteNode(imageNamed: "paciosdev")
+        ball.physicsBody = SKPhysicsBody(circleOfRadius: ball.size.width / 2.7)
+        ball.physicsBody?.restitution = 0.4
+        ball.position = location
+        addChild(ball)
+    }
+}
 
 struct ContentView: View {
+    
+    var scene: SKScene{
+        let scene = GameScene()
+        scene.size = CGSize(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        return scene
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white.opacity(0.8), Color.blue]), startPoint: .top, endPoint: .bottom).edgesIgnoringSafeArea(.all)
+            
+            VStack{
+                SpriteView(scene: scene)
+                    .frame(width: 320, height: 650)
+            }
+        }
     }
 }
 
